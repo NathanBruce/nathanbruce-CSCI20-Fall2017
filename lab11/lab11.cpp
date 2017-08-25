d45418b5094528d1160a0f80bf2e720f049b9d07
Nathan Bruce <nbruce003@student.butte.edu>
2017-08-25 01:47:27 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,14 @@
+have the computer choose between 1 and 100
+create a variable to act as a counter
+have the user choose a number
+if the user chooses the correct number, output "you got it!" and end program
+
+if the input is not a number, say "hey buddy, use a number" and restart program
+
+if the number is higher than the user's input, say higher
+add 1 to the counter
+
+if the number is lower than the user's input, say lower
+add 1 to the counter
+
+if the counter reaches 10, end the program and output "You lose."
