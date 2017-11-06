9cf41927c6e373334022cecd8fda4fc30a99fa2c
Nathan Bruce <nbruce003@student.butte.edu>
2017-11-06 07:54:03 +0000

    Lab3.3 submission

--- lab31/lab31.cpp
+++ lab31/lab31.cpp
@@ -2,15 +2,38 @@
 #include <string>
 using namespace std;
 
+/*
+void Sort_Prices(prices[]) {
+    placeholder = 0;
+    for (i = 0; i < 6; ++i){
+        if (prices[i] > prices[i + 1]){
+            placeholder = prices[i];
+            prices[i + 1] = prices[i];
+            prices[i] = prices[i+1];
+            prices[+1] = placeholder;
+        }}};
+        
+*/
+
+
+
+/*
+void Cheapest_Plan(int prices) {
+   
+   
+};
+
+void Print_Result(string names, int prices){
+   
+};
+*/
+
 
 
 int main() {
 
-
-
 int smart_phones = 0;
 int data_usageGB = 0;
-bool phone_type = true;
 int dumb_phone = 0;
 int tablet_amount = 0;
 
@@ -31,68 +54,72 @@ int Spr_unlim_cost = 0;
 int Ver_unlim_cost = 0;
 int ATT_fam_cost = 0;
 
+int CONST = 6;
+int prices[CONST];
+string products[CONST];
 
 
-if (phone_type == true) {                                        //
-  cout << endl << "How many smart phones would you like? ";      //
-  cin >> smart_phones;                                           //
-//  cout << endl << "You want " << smart_phones << " smart phones. " << endl << endl;
+//collect inputs from the user for quantity of: smart phones, dumb phones, tablet, and GB's of data
+//this will be how much the customer is looking to purchase
 
+  cout << endl << "How many smart phones would you like? ";      
+  cin >> smart_phones;                                           
+
+  cout << endl << "How many dumb phones do you want? ";        
+  cin >> dumb_phone;                                           
+                                                                 
+  cout << endl << "How many tablets do you want? ";             
+  cin >> tablet_amount;                                         
+
+  cout << endl << "How many GB of data would you like for your monthly plan? ";      
+  cin >> data_usageGB;                
+
+
+//this loop will determine the customers need based on their data usage
+//it will quote differnt prices based on the plan (20,30,60,etc)
+//whichever is chosen will be stored in a variable for calculating score later on
+//if not a valid number, tells user to enter valid number
+
+if (data_usageGB < 2 && data_usageGB > 0) {                                        
+   cout << "1gb" << endl;                                                          
+   ATT_fam_gb_cost = 30;
 }
 
-if (phone_type == true) {                                        //
-    cout << endl << "How many dumb phones do you want? ";        //
-    cin >> dumb_phone;                                           //
-//    cout << endl << " you want " << dumb_phone << " dumb phones." << endl << endl;
+   else if ((data_usageGB <= 3) && (data_usageGB > 1)) {                              
+      cout << "3gb " << endl;                                                         
+      ATT_fam_gb_cost = 40;
+   }
+   
+   else if ((data_usageGB <= 6) && (data_usageGB > 3)) {                              
+      cout << "6gb " << endl;                                                         
+      ATT_fam_gb_cost = 60;                                                           
+   }
+   
+   else if ((data_usageGB <= 10) && (data_usageGB > 6)) {
+      cout << "10gb " << endl;
+      ATT_fam_gb_cost = 80;
+   }
+   
+   else if ((data_usageGB <= 16) && (data_usageGB > 10)) {
+      cout << "16gb " << endl;
+      ATT_fam_gb_cost = 90;
+   }
+   
+   else if ((data_usageGB <= 25) && (data_usageGB > 16)) {
+      cout << "25gb " << endl;
+      ATT_fam_gb_cost = 110;
 }
 
-if (phone_type == true) {                                        //
-   cout << endl << "How many tablets do you want? ";             //
-   cin >> tablet_amount;                                         //
+   else {
+      data_usageGB = 0;
+      cout << "Enter a valid number.. " << endl;
 }
 
-
-cout << endl << "How many GB of data would you like for your monthly plan? ";      //
-cin >> data_usageGB;                
-
-if (data_usageGB < 2 && data_usageGB > 0) {                                        //
-   cout << "1gb" << endl;                                                          //
-   ATT_fam_gb_cost = 30;                                                           //
-}
-
-//else if ((data_usageGB <= 2) && (data_usageGB > 1)) {
-//   cout << "2gb" << endl;
-//}
-else if ((data_usageGB <= 3) && (data_usageGB > 1)) {                              //
-   cout << "3gb " << endl;                                                         //
-   ATT_fam_gb_cost = 40;                                                           //
-}
-else if ((data_usageGB <= 6) && (data_usageGB > 3)) {                              //
-   cout << "6gb " << endl;                                                         //
-   ATT_fam_gb_cost = 60;                                                           //
-}
-else if ((data_usageGB <= 10) && (data_usageGB > 6)) {
-   cout << "10gb " << endl;
-   ATT_fam_gb_cost = 80;
-}
-else if ((data_usageGB <= 16) && (data_usageGB > 10)) {
-   cout << "16gb " << endl;
-   ATT_fam_gb_cost = 90;
-}
-else if ((data_usageGB <= 25) && (data_usageGB > 16)) {
-   cout << "25gb " << endl;
-   ATT_fam_gb_cost = 110;
-}
-
-else {
-    data_usageGB = 0;
-    cout << "Enter a valid number.. " << endl;
-}
-
-
+//outputs the customers choices
 cout << endl << "smart phones: " << smart_phones << endl;
 cout << "dumb phones: " << dumb_phone << endl;
-cout << "data plan: " << data_usageGB << " GB" << endl << endl;
+cout << "data plan: " << data_usageGB << " GB" << endl;
+
 
 //create alterable variables to calculate the price for each tree
 
@@ -103,137 +130,237 @@ Ver_unlim_phones = smart_phones + dumb_phone;
 Spr_unlim_phones = smart_phones + dumb_phone;
 Sprint_2gb_phones = smart_phones + dumb_phone;
 tablet_cost = tablet_amount * tablet_cost;
-cout << "Tablet cost: " << tablet_cost << endl;
-//=============================ATT Unlimited Plus Plan===========================
+cout << "Tablet cost: $" << tablet_cost << endl << endl;
 
+
+//starts the ATT Unlimited Plus plan loop
+//cost will be calculated based on how many phones/tablets/etc the user chooses and the cost
+//output total cost
+//=============================ATT Unlimited Plus Plan===========================
 if (ATT_plus_phones == 1) {
       ATT_plus_cost = (90 ) + (tablet_cost);
       cout << "ATT Unlimited Plus Plan: " << ATT_plus_cost << endl;
-    
+      prices[0] = ATT_plus_cost;      
+      products[0] = "ATT Unlimited Plus Plan: ";       
 }
 
    else if (ATT_plus_phones == 2) {
        ATT_plus_cost = (145)  + (tablet_cost);
        cout << "ATT Unlimited Plus Plan: " << ATT_plus_cost << endl;
+       prices[0] = ATT_plus_cost;
+      products[0] = "ATT Unlimited Plus Plan: ";
 }
 
    else if ((ATT_plus_phones > 2) && (ATT_plus_phones <= 10)) {
        ATT_plus_phones = ATT_plus_phones - 2;
        ATT_plus_cost = 145 + (ATT_plus_phones * 20) + (tablet_cost);
        cout << "ATT Unlimited Plus Plan:  " << ATT_plus_cost << endl;
-}
-
-   else {
-       cout << "Enter a valid amount.. " << endl;
-}
-
-//============================ATT Unlimited Choice Plan=========================================
- if (ATT_unlim_phones == 1) {
-      ATT_unlim_cost = 60  + tablet_cost;
-      cout << "ATT Unlimited Choice Plan: " << ATT_unlim_cost << endl;
-    
-}
-
-   else if (ATT_unlim_phones == 2) {
-       ATT_unlim_cost = 115 + tablet_cost;
-       cout << "ATT Unlimited Choice Plan: " << ATT_unlim_cost << endl;
-}
-
-   else if ((ATT_unlim_phones > 2) && (ATT_unlim_phones <= 10)) {
-       ATT_unlim_phones = ATT_unlim_phones - 2;
-       ATT_unlim_cost = 115 + (ATT_unlim_phones * 20) + tablet_cost;
-       cout << "ATT Unlimited Choice Plan: " << ATT_unlim_cost << endl;
-}
-
-   else {
-       cout << "Enter a valid amount.. " << endl;
-}
-
-
-//===============================ATT Family Plans=========================================
- if (data_usageGB == 0) {
-      ATT_fam_cost = (ATT_fam_phones * 20) + (tablet_cost);
-      cout << "ATT Family Plans: " << ATT_fam_cost << endl;
- 
-}
-   else if ((data_usageGB < 2) && (data_usageGB > 0)) {
-       ATT_fam_cost = (ATT_fam_phones * 20)  + 30 + (tablet_cost);
-       cout << "ATT Family Plans: " << ATT_fam_cost << endl;      
-}
-   else if ((data_usageGB <= 3) && (data_usageGB > 2)) {
-       ATT_fam_cost = (ATT_fam_phones * 20) + 40 + (tablet_cost);
-       cout << "ATT Family Plans: " << ATT_fam_cost << endl;
-}
-   else if ((data_usageGB <= 6) && (data_usageGB > 3)) {
-       ATT_fam_cost = (ATT_fam_phones * 20) + 60 + (tablet_cost);
-       cout << "ATT Family Plans: " << ATT_fam_cost << endl;      
-}
-   else if ((data_usageGB <= 10) && (data_usageGB > 6)) {
-       ATT_fam_cost = (ATT_fam_phones * 20) + 80 + (tablet_cost);
-       cout << "ATT Family Plans: " << ATT_fam_cost << endl;      
-}
-   else if ((data_usageGB <= 16) && (data_usageGB > 10)) {
-       ATT_fam_cost = (ATT_fam_phones * 20) + 90 + (tablet_cost);
-       cout << "ATT Family Plans: " << ATT_fam_cost << endl;      
-}
-   else if ((data_usageGB <= 25) && (data_usageGB > 16)) {
-       ATT_fam_cost = (ATT_fam_phones * 20) + 110 + (tablet_cost);
-       cout << "ATT Family Plans: " << ATT_fam_cost << endl;      
-}
-
-   else {
-       cout << "Enter a valid amount.. " << endl;
-}
-//===============================Verizon Unlimited====================================
- if (Ver_unlim_phones == 1) {
-      Ver_unlim_cost = 80 + (tablet_amount * 10) + (Ver_unlim_phones * 20);
-      cout << "ATT Unlimited Choice Plan: " << Ver_unlim_cost << endl;
-    
-}
-
-   else if (Ver_unlim_phones == 2) {
-       Ver_unlim_cost = 110 + (tablet_amount * 10) + (Ver_unlim_phones * 20);
-       cout << "ATT Unlimited Choice Plan: " << Ver_unlim_cost << endl;
-}
-   else if (Ver_unlim_phones >= 3) {
-       Ver_unlim_cost = 110 + (tablet_amount * 10) + (Ver_unlim_phones * 20);
+       prices[0] = ATT_plus_cost;
+      products[0] = "ATT Unlimited Plus Plan: ";      
    }
 
    else {
        cout << "Enter a valid amount.. " << endl;
 }
-//===============================Sprint Unlimited====================================
- if (Spr_unlim_phones == 1) {
-      Spr_unlim_cost = 60;
-      cout << "Sprint Unlimited Plan: " << Spr_unlim_cost << endl;
-    
-}
 
-   else if (Spr_unlim_phones == 2) {
-      Spr_unlim_cost = 100;
-      cout << "Sprint Unlimited Plan: " << Spr_unlim_cost << endl;
-}
-   else if (Spr_unlim_phones >= 3) {
-      Spr_unlim_cost = 100 + (Spr_unlim_phones * 30);
-      cout << "Sprint Unlimited Plan: " << Spr_unlim_cost << endl;
-   }   
-   else {
-      cout << "Enter a valid amount.. " << endl;
-}
-//===============================Sprint 2GB Plan====================================
- if (Sprint_2gb_phones >= 1) {
-      Sprint_2gb_cost = 40 * Sprint_2gb_phones;
-      cout << "Spring 2GB Plan:  " << Sprint_2gb_cost << endl;
-    
-}
+//starts the ATT Unlimited Choice loop
+//cost will be calculated based on how many phones/tablets/etc the user chooses and the cost
+//output total cost
+//============================ATT Unlimited Choice Plan=========================================
+ if (ATT_unlim_phones == 1) {
+      ATT_unlim_cost = 60  + tablet_cost;
+      cout << "ATT Unlimited Choice Plan: " << ATT_unlim_cost << endl;
+      prices[1] = ATT_unlim_cost;    
+      products[1] = "ATT Unlimited Choice Plan: ";
+ }
+
+   else if (ATT_unlim_phones == 2) {
+       ATT_unlim_cost = 115 + tablet_cost;
+       cout << "ATT Unlimited Choice Plan: " << ATT_unlim_cost << endl;
+      prices[1] = ATT_unlim_cost;
+      products[1] = "ATT Unlimited Choice Plan: " ;      
+   }
+
+   else if ((ATT_unlim_phones > 2) && (ATT_unlim_phones <= 10)) {
+       ATT_unlim_phones = ATT_unlim_phones - 2;
+       ATT_unlim_cost = 115 + (ATT_unlim_phones * 20) + tablet_cost;
+       cout << "ATT Unlimited Choice Plan: " << ATT_unlim_cost << endl;
+      prices[1] = ATT_unlim_cost;
+      products[1] = "ATT Unlimited Choice Plan: " ;      
+   }
 
    else {
        cout << "Enter a valid amount.. " << endl;
 }
 
+//starts the ATT Family Plan loop
+//cost will be calculated based on how many phones/tablets/etc the user chooses and the cost
+//output total cost
+//===============================ATT Family Plans=========================================
+ if (data_usageGB == 0) {
+      ATT_fam_cost = (ATT_fam_phones * 20) + (tablet_cost);
+      cout << "ATT Family Plans: " << ATT_fam_cost << endl;
+      prices[2] = ATT_fam_cost; 
+      products[2] = "ATT Family Plans: " ;
+ }
+ 
+   else if ((data_usageGB <= 2) && (data_usageGB > 0)) {
+       ATT_fam_cost = (ATT_fam_phones * 20)  + 30 + (tablet_cost);
+       cout << "ATT Family Plans: " << ATT_fam_cost << endl;      
+      prices[2] = ATT_fam_cost;
+      products[2] = "ATT Family Plans: " ;
+}
+   
+   else if ((data_usageGB <= 3) && (data_usageGB > 2)) {
+       ATT_fam_cost = (ATT_fam_phones * 20) + 40 + (tablet_cost);
+       cout << "ATT Family Plans: " << ATT_fam_cost << endl;
+      prices[2] = ATT_fam_cost;
+      products[2] = "ATT Family Plans: " ;
+}
+   
+   else if ((data_usageGB <= 6) && (data_usageGB > 3)) {
+       ATT_fam_cost = (ATT_fam_phones * 20) + 60 + (tablet_cost);
+       cout << "ATT Family Plans: " << ATT_fam_cost << endl;      
+      prices[2] = ATT_fam_cost;
+      products[2] = "ATT Family Plans: " ;
+}
+   
+   else if ((data_usageGB <= 10) && (data_usageGB > 6)) {
+       ATT_fam_cost = (ATT_fam_phones * 20) + 80 + (tablet_cost);
+       cout << "ATT Family Plans: " << ATT_fam_cost << endl;      
+      prices[2] = ATT_fam_cost;
+      products[2] =  "ATT Family Plans: " ;
+}
+   
+   else if ((data_usageGB <= 16) && (data_usageGB > 10)) {
+       ATT_fam_cost = (ATT_fam_phones * 20) + 90 + (tablet_cost);
+       cout << "ATT Family Plans: " << ATT_fam_cost << endl;      
+      prices[2] = ATT_fam_cost;
+      products[2] = "ATT Family Plans: " ;
+}
+   
+   else if ((data_usageGB <= 25) && (data_usageGB > 16)) {
+       ATT_fam_cost = (ATT_fam_phones * 20) + 110 + (tablet_cost);
+       cout << "ATT Family Plans: " << ATT_fam_cost << endl;      
+      prices[2] = ATT_fam_cost;
+      products[2] =  "ATT Family Plans: " ;
+}
+   
+
+   else {
+       cout << "Enter a valid amount.. " << endl;
+}
+
+//starts the Verizon unlimited plan loop
+//cost will be calculated based on how many phones/tablets/etc the user chooses and the cost
+//output total cost
+//===============================Verizon Unlimited====================================
+ if (Ver_unlim_phones == 1) {
+      Ver_unlim_cost = 80 + (tablet_amount * 10) + (Ver_unlim_phones * 20);
+      cout << "Verizon Unlimited Plan: " << Ver_unlim_cost << endl;
+      prices[3] = Ver_unlim_cost;      
+      products[3] = "Verizon Unlimited Plan: " ;  
+ }
+
+   else if (Ver_unlim_phones >= 2) {
+       Ver_unlim_cost = 110 + (tablet_amount * 10) + (Ver_unlim_phones * 20);
+       cout << "Verizon Unlimited Plan: " << Ver_unlim_cost << endl;
+      prices[3] = Ver_unlim_cost;
+      products[3] = "Verizon Unlimited Plan: " ;  
+}
+   
+//   else if (Ver_unlim_phones >= 3) {
+ //      Ver_unlim_cost = 110 + (tablet_amount * 10) + (Ver_unlim_phones * 20);
+ //     prices[3] = Ver_unlim_cost;
+//   }
+
+   else {
+       cout << "Enter a valid amount.. " << endl;
+}
+
+//starts the Sprint Unlimited Plan loop
+//cost will be calculated based on how many phones/tablets/etc the user chooses and the cost
+//output total cost
+//===============================Sprint Unlimited====================================
+ if (Spr_unlim_phones == 1) {
+      Spr_unlim_cost = 60;
+      cout << "Sprint Unlimited Plan: " << Spr_unlim_cost << endl;
+      prices[4] = Spr_unlim_cost;
+      products[4] = "Sprint Unlimited Plan: " ;
+}
+ 
+   else if (Spr_unlim_phones == 2) {
+      Spr_unlim_cost = 100;
+      cout << "Sprint Unlimited Plan: " << Spr_unlim_cost << endl;
+      prices[4] = Spr_unlim_cost;
+      products[4] = "Sprint Unlimited Plan: " ;
+   }
+   
+   else if (Spr_unlim_phones >= 3) {
+      Spr_unlim_cost = 100 + (Spr_unlim_phones * 30);
+      cout << "Sprint Unlimited Plan: " << Spr_unlim_cost << endl;
+      prices[4] = Spr_unlim_cost;
+      products[4] = "Sprint Unlimited Plan: " ;
+   }  
+   
+   else {
+      cout << "Enter a valid amount.. " << endl;
+}
+
+//starts the Sprint 2GB Plan loop
+//cost will be calculated based on how many phones/tablets/etc the user chooses and the cost
+//output total cost
+//===============================Sprint 2GB Plan====================================
+ if (Sprint_2gb_phones >= 1) {
+      Sprint_2gb_cost = 40 * Sprint_2gb_phones;
+      cout << "Spring 2GB Plan:  " << Sprint_2gb_cost << endl;
+      prices[5] = Sprint_2gb_cost;
+      products[5] = "Spring 2GB Plan:  " ;
+}
+   else {
+       cout << "Enter a valid amount.. " << endl;
+}
+//========================================================OUTPUT=======================
+
+int i = 0;
+for (i=0; i < 5; ++i){
+    cout << prices[i] << endl;
+}
+        
+/*       for (j = 0; j < 6; ++i){
+           if (prices[i] < prices[j]){
+              prices[i] = ph;
+              prices[j] = prices[i];
+              ph = prices[j];
+           }
+ */       
+//        }}
+/*}};
 
 
-cout <<"DONE";
-    
+
+cout <<"\nDONE. Thank you!\n";
+
+int i = 0;
+for (i = 0; i <= 5; ++i){
+   cout << products[i] << " " << prices[i] << endl;
+   cout << "prices are: " << Sort_Prices(prices[i]);
+   
+}
+
+
+
+*/
+
+
+/*
+int mn = 0;
+int i = 0;
+min = prices[0];
+
+for (i = 0; i <= 5; ++i){
+   Sort_Prices(min);
+}
+*/
 return 0;
-} 
+}
--- lab33/lab33.cpp
+++ lab33/lab33.cpp
@@ -1,51 +1,67 @@
 #include <iostream>
 #include <time.h>  //import time library for srand
-
 using namespace std;
 
 
 int main() {
 
+
+//declare the variables: user's guess, computer generated number, and counter
 int user_num = 0;    
 int computer_num = 0;
 int i = 0;
 
 
+//creates a random number generator using time as the seed
+//the generator will create numbers between 1 and 50
 srand (time(NULL));
 computer_num = rand() % 50 + 1;
 cout << computer_num << "\n";
-    
+
+//create a while loop that will initiate while user number is not same as the computer generated one
+// AND when the countdown 'i' has not reached zero
 while (user_num != computer_num && i != 0); {
     
-
+//countdown timer will iterate 5 times
+//this will let the user know how many guesses they have left
+//and collect the users number guess
    for (i = 5; i > 0; i-- ) {
        cout << "You have " << i << " guesses left. \n";
        cout << "\nEnter a value: ";
+       cin >> user_num;
 
-    cin >> user_num;
+//if the user's number is higher, will ouutput 'too high'
+//if lower, it will output 'too low'
+//if the number is the same, inform the user that they have won and break the loop
        if (user_num > computer_num){
-           cout << "Too High. \n";
+          cout << "Too High. \n";
     }
-      else if (user_num < computer_num){
-           cout << "Too Low. \n";
+          else if (user_num < computer_num){
+              cout << "Too Low. \n";
 
-   }
-      else if (user_num == computer_num) {
-          cout << "\n\nAyyyy, you got it.. \n" << user_num << " = " << computer_num << "\n\n";
-          break;
+    }
+          else if (user_num == computer_num) {
+              cout << "\n\nAyyyy, you got it.. \n" << user_num << " = " << computer_num << "\n\n";
+              break;
 }
-    
 }}
+
+
+//if the user's guessed number is equal to the randomly generated one, print out a congratulations message
+//if it does not, penalize them for their failures with social mockery
 if (user_num == computer_num){
     cout << "**********************************************" << endl;
     cout << "Congrats! you won.                           *\n";
     cout << "**********************************************" << endl;
 }
-else {
-    cout << "-----------------------------------------------";
-    cout << "\nYou suck, loser. \n";
-    cout << "-----------------------------------------------";
+   else {
+       cout << "-----------------------------------------------";
+       cout << "\nYou suck, loser. \n";
+       cout << "-----------------------------------------------";
 }
+
+
+//regardless of if they guess right or not, enter an exit message
 cout << "\nGame ending...";
 
 return 0;    
